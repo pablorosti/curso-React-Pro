7c2eccfe725b0d5d778fc785dda7f2cffe1f337c
Rosti <pablorostirollatenis@gmail.com>
2021-11-30 20:15:17 -0300

    .

--- src/02-compound-component-pattern/pages/ShoppingPage.tsx
+++ src/02-compound-component-pattern/pages/ShoppingPage.tsx
@@ -6,12 +6,20 @@ import {
   ProductButtons,
 } from "../components/index";
 import "../styles/custom-styles.css";
+import { Product } from "../interfaces/IProduct";
 
 const product = {
   id: "1",
   title: "coffee-mug",
   img: "./coffee-mug.png",
 };
+const product2 = {
+  id: "2",
+  title: "coffee-mug 2",
+  img: "./coffee-mug2.png",
+};
+
+const products: Product[] = [product, product2];
 
 export const ShoppingPage = () => {
   return (
@@ -24,15 +32,30 @@ export const ShoppingPage = () => {
           flexWrap: "wrap",
         }}
       >
-        <ProductCard product={product}>
+        {products.map((product) => (
+          <ProductCard product={product} className="bg-dark" key={product.id}>
+            <ProductImage />
+            <ProductTitle className="text-white" />
+            <ProductButtons className="text-white" />
+          </ProductCard>
+        ))}
+      </div>
+      <div className="shopping-cart">
+        <ProductCard
+          product={product2}
+          className="bg-dark"
+          style={{ width: "100px" }}
+        >
           <ProductImage />
-          <ProductTitle />
-          <ProductButtons />
+          <ProductButtons className="text-white" />
         </ProductCard>
 
-        <ProductCard product={product} className="bg-dark">
+        <ProductCard
+          product={product}
+          className="bg-dark"
+          style={{ width: "100px" }}
+        >
           <ProductImage />
-          <ProductTitle className="text-white" />
           <ProductButtons className="text-white" />
         </ProductCard>
       </div>
