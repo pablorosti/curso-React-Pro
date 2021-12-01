f1336cb0e2f4b4a8b95e9e701a02b6317257769d
Rosti <pablorostirollatenis@gmail.com>
2021-12-01 10:15:08 -0300

    .

--- src/02-compound-component-pattern/pages/ShoppingPage.tsx
+++ src/02-compound-component-pattern/pages/ShoppingPage.tsx
@@ -6,68 +6,22 @@ import {
   ProductButtons,
 } from "../components/index";
 import "../styles/custom-styles.css";
-import { Product } from "../interfaces/IProduct";
 
 const product = {
   id: "1",
   title: "coffee-mug",
   img: "./coffee-mug.png",
 };
-const product2 = {
-  id: "2",
-  title: "coffee-mug 2",
-  img: "./coffee-mug2.png",
-};
-
-const products: Product[] = [product, product2];
-
-interface ProductInCart extends Product {
-  count: number;
-}
 
 export const ShoppingPage = () => {
-  const [shoppingCart, setShoppingCart] = useState<{
-    [key: string]: ProductInCart;
-  }>({
-    "1": { ...product, count: 10 },
-    "2": { ...product2, count: 2 },
-  });
-
-  console.log(shoppingCart);
   return (
     <div>
       <h1>Shopping</h1>
       <hr />
-      <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-        }}
-      >
-        {products.map((product) => (
-          <ProductCard product={product} className="bg-dark" key={product.id}>
-            <ProductImage />
-            <ProductTitle className="text-white" />
-            <ProductButtons className="text-white" />
-          </ProductCard>
-        ))}
-      </div>
-      <div className="shopping-cart">
-        <ProductCard
-          product={product2}
-          className="bg-dark"
-          style={{ width: "100px" }}
-        >
+      <div>
+        <ProductCard product={product} className="bg-dark" key={product.id}>
           <ProductImage />
-          <ProductButtons className="text-white" />
-        </ProductCard>
-
-        <ProductCard
-          product={product}
-          className="bg-dark"
-          style={{ width: "100px" }}
-        >
-          <ProductImage />
+          <ProductTitle className="text-white" />
           <ProductButtons className="text-white" />
         </ProductCard>
       </div>
